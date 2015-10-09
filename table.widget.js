9d1286d074a68f69c53730b4dfb618fae581f2f7
Alexandr <a.s.shevakov@gmail.com>
2015-10-09 17:28:15 +0300

    color

--- table.widget.js
+++ table.widget.js
@@ -2,7 +2,7 @@
 
 	var TableWidget = function(settings) {
 		var self = this;
-		var $table = $('<table style="width:100%; height:100%; font-size:11px;"><thead style="background:#CFCCCB;"><tr><th>Name</th><th>Model</th><th>Status</th><th>Low battery</th></tr></thead><tbody></tbody></table>');
+		var $table = $('<table style="width:100%; height:100%; font-size:11px; text-align: center;"><thead style="background:#CFCCCB;"><tr><th>Name</th><th>Model</th><th>Status</th><th>Low battery</th></tr></thead><tbody></tbody></table>');
 		var currentSettings = [];
 		self.render = function(containerElement) {
 
@@ -38,7 +38,7 @@
       };
       if(settingName == 'select_drone'){
       	$table.find('tbody tr').css('background-color','');
-      	$table.find('tbody tr:nth-child('+newValue+')').css('background-color','red');
+      	$table.find('tbody tr:nth-child('+newValue+')').css('background-color','rgb(87, 124, 159)');
       }
 		}
 
@@ -50,7 +50,7 @@
 	function rowWriter(json) {
 	  var tr = '',
 	  		indicator_light_off = '"border-radius: 50%; width: 10px; height: 10px; border: 2px solid rgb(61, 61, 61); background-color: rgb(34, 34, 34); margin: auto"',
-				indicator_light_on = '"border-radius: 50%; width: 10px; height: 10px; border: 2px solid rgb(61, 61, 61); background-color: rgb(255, 199, 115); box-shadow: rgb(255, 153, 0) 0px 0px 15px; margin: auto"',
+				indicator_light_on = '"border-radius: 50%; width: 10px; height: 10px; border: 2px solid #FDF1DF; background-color: rgb(255, 199, 115); box-shadow: rgb(255, 153, 0) 0px 0px 15px; margin: auto"',
 				on_off = json['low_battery']? indicator_light_on : indicator_light_off;
 	  tr = '<td>' + json['name'] + '</td><td>' + json['model'] + '</td><td>' + json['status'] + '</td><td><div style='+on_off+'></div>' + '</td>';
 	  return '<tr>' + tr + '</tr>';
